1b90057783418a0736bbd83ed3ad1e78b62f197c
sindreeo <sindreeo@stud.ntnu.no>
2020-03-10 19:28:07 +0100

    Adds player ond point classes

--- core/src/com/mygdx/game/models/PlayerModel.java
+++ core/src/com/mygdx/game/models/PlayerModel.java
@@ -1,4 +1,38 @@
 package com.mygdx.game.models;
 
+
+import com.badlogic.gdx.graphics.Color;
+import java.lang.reflect.Array;
+import java.util.List;
+import  com.mygdx.game.models.Point;
+
 public class PlayerModel {
+    String username;
+    Color color;
+    LineModel line;
+    boolean active;
+
+    PlayerModel(String username, Color color){
+        this.username = username;
+        this.color = color;
+        this.line = new Line();
+        this.active = true;
+    }
+
+    public List<Point> getLinePoints(){
+        return this.line.points;
+    }
+
+    public void setNotActive(){
+        this.active = false;
+    }
+    public void setActive(){
+        this.active = true;
+    }
+
+    public void setNewPoint(int x, int y){
+        Point point = new Point(x,y);
+        this.line.addPoint(point);
+    }
+
 }
