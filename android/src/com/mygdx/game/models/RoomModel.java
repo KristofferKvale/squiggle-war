e91c51ca6e5f695e22b4d2c20a1b90daf656438f
simen <simenberghansen97@gmail.com>
2020-03-16 13:44:08 +0100

    Start of RoomModel

---
+++ android/src/com/mygdx/game/models/RoomModel.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.models;
+
+import com.mygdx.game.Game;
+
+public class RoomModel {
+    //Links up with firebase and creates game and player models
+    private Game game;
+
+    public RoomModel() {
+        game = new Game();
+        //?
+    }
+}
