fba50aa8affafc20f22be762800944616575f242
simen <simenberghansen97@gmail.com>
2020-03-27 12:44:15 +0100

    Made changes to RoomModel

--- android/src/com/mygdx/game/models/RoomModel.java
+++ android/src/com/mygdx/game/models/RoomModel.java
@@ -1,13 +1,30 @@
 package com.mygdx.game.models;
 
 import com.mygdx.game.Game;
+import com.mygdx.game.models.Config;
+import java.util.List;
+import com.badlogic.gdx.math.Vector2;
 
 public class RoomModel {
     //Links up with firebase and creates game and player models
     private Game game;
+    private List<PlayerModel> players;
+    Config config = new Config();
+
 
     public RoomModel() {
         game = new Game();
-        //?
     }
+
+    /*
+    public AddPlayer() {
+        //Adds player to list of players inside the room
+        //players.add(new PlayerModel(config.username, random_color, new Vector2(200,200)));
+    }*/
+
+    //Starts countdown when two players enter room
+    //Start game when finished
+    //(Vet ikke helt hvordan det skal gjøres)
+    // public startGame()
+
 }
