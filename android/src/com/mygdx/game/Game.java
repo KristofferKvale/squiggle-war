90be2f244b727224aee8d2b55f247b97ec173853
Jon Rennan <jonren@stud.ntnu.no>
2020-03-29 15:35:55 +0200

    Add method Game.randomPosition() which returns a random vector with position atleast 100 away from walls

--- android/src/com/mygdx/game/Game.java
+++ android/src/com/mygdx/game/Game.java
@@ -5,45 +5,51 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.google.firebase.database.FirebaseDatabase;
 import com.mygdx.game.models.Config;
 import com.mygdx.game.views.GameStateManager;
 import com.mygdx.game.views.GameView;
 import com.mygdx.game.views.UsernameView;
 
-
 public class Game extends ApplicationAdapter {
 
-	public static final int WIDTH = Gdx.app.getGraphics().getWidth();
-	public static final int HEIGHT = Gdx.app.getGraphics().getHeight();
-	public static final int SPEED = 2;
-	public static final double ROTATION_SPEED = 0.03;
+    public static final int WIDTH = Gdx.app.getGraphics().getWidth();
+    public static final int HEIGHT = Gdx.app.getGraphics().getHeight();
+    public static final int SPEED = 2;
+    public static final double ROTATION_SPEED = 0.03;
 
-	public static final String TITLE = "Squiggle War";
+    public static final String TITLE = "Squiggle War";
 
-	private GameStateManager gsm;
-	private SpriteBatch batch;
-	public Config config;
-	
-	@Override
-	public void create () {
-		config = Config.getInstance();
-		batch = new SpriteBatch();
-		gsm = new GameStateManager();
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		gsm.push(new GameView(gsm));
-	}
+    private GameStateManager gsm;
+    private SpriteBatch batch;
+    public Config config;
 
-	@Override
-	public void render () {
+    @Override
+    public void create() {
+        config = Config.getInstance();
+        batch = new SpriteBatch();
+        gsm = new GameStateManager();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new GameView(gsm));
+    }
 
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-	}
+    @Override
+    public void render() {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(batch);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    public Vector2 randomPosition() {
+        int x = (int) (Math.random() * (WIDTH - 200)) + 100;
+        int y = (int) (Math.random() * (HEIGHT - 200)) + 100;
+        return new Vector2(x, y);
+    }
 }
