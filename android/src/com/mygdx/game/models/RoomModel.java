f30803b57ff69412150530bbc7fc7d4b567773fc
jonren <jonren@stud.ntnu.no>
2020-04-09 11:29:42 +0200

    Fix color going out of bounds

--- android/src/com/mygdx/game/models/RoomModel.java
+++ android/src/com/mygdx/game/models/RoomModel.java
@@ -135,7 +135,7 @@ public class RoomModel {
 
 
     private Color randomColor(Integer min, Integer max){
-        Integer i = (int)(Math.random() * (max - min + 1) + min);
+        Integer i = (int)(Math.random() * (max - min) + min);
         return colors.get(i);
     }
 
