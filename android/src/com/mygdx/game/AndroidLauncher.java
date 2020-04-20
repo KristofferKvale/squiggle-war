3b667c60997f002769b2968e56ee48b1f6d1c7b5
Adrian-Desktop <aspleym@gmail.com>
2020-04-20 11:09:06 +0200

    Added a test api for AdMob.

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,17 +1,36 @@
 package com.mygdx.game;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.Game;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class AndroidLauncher extends AndroidApplication {
+
+	public static InterstitialAd mInterstitialAd;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		final AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+		MobileAds.initialize(this, new OnInitializationCompleteListener() {
+			@Override
+			public void onInitializationComplete(InitializationStatus initializationStatus) {
+				Log.d("AdMob", "Done initializing");
+			}
+		});
+
+		mInterstitialAd = new InterstitialAd(this);
+		mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
 		initialize(new Game(), config);
 	}
 }
