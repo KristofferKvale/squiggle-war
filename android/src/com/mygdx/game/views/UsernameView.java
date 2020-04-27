dd86bd0878a5e8ae96c18d9f0707c5660c8978ae
Jon Rennan <jonren@stud.ntnu.no>
2020-04-27 12:35:02 +0200

    Remove unused code

--- android/src/com/mygdx/game/views/UsernameView.java
+++ android/src/com/mygdx/game/views/UsernameView.java
@@ -1,21 +1,13 @@
 package com.mygdx.game.views;
 
-import androidx.annotation.NonNull;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.mygdx.game.Game;
 import com.mygdx.game.models.Config;
 
 public class UsernameView extends State {
@@ -30,18 +22,6 @@ public class UsernameView extends State {
 
         super(gsm);
         g = gsm;
-/*        cam.setToOrtho(false, Game.WIDTH / 2f, Game.HEIGHT / 2f);
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-      //  Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-        TextField.TextFieldStyle style = new TextField.TextFieldStyle();
-        style.font = new BitmapFont();
-        style.fontColor = Color.CHARTREUSE;
-        usernameField = new TextField("", style);
-        usernameField.setPosition(cam.position.x,cam.position.y);
-        usernameField.setSize(300,40);
-        stage.addActor(this.usernameField);
-        this.t = new Texture("badlogic.jpg");*/
         this.config= config;
         Gdx.input.getTextInput(new Input.TextInputListener() {
 
@@ -57,16 +37,6 @@ public class UsernameView extends State {
                 System.out.println("ups");
             }
         },"Username","","");
-      /*  TextButton  submitButton = new TextButton("Submit", skin);
-        submitButton.setPosition(100, 100);
-        submitButton.setSize(300,60);
-        submitButton.addListener( new ClickListener(){
-            @Override
-            public void touchUp(InputEvent e, float x, float y, int point, int button) {
-               // gsm.push(new LobbySelectionView(gsm);
-            }
-        });
-        stage.addActor(submitButton);*/
     }
 
 
@@ -108,7 +78,6 @@ public class UsernameView extends State {
         sb.setProjectionMatrix(this.cam.combined);
 
         sb.begin();
-        //sb.draw(this.t,cam.position.x,cam.position.y);
         sb.end();
 
 
